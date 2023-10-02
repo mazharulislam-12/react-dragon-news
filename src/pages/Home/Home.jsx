d394af6ff6a4d0f2cba3b662d7dabd607d151ed4
mazharulislam-12 <505mazharulislam@gmail.com>
2023-10-02 09:16:53 -0700

    components create

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from '../Shared/Header/Header';
+import Navbar from '../Shared/Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
-            <h2 className='text-2xl'>This is home</h2>
+            <Header></Header>
+            <Navbar></Navbar>
+            <h2 className='text-2xl font-poppins'>This is home</h2>
         </div>
     );
 };
