303f20991883a4c9a7d253646720cb3f43d53c1f
mazharulislam-12 <505mazharulislam@gmail.com>
2023-10-02 09:42:20 -0700

    header section crate

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import Header from '../Shared/Header/Header';
 import Navbar from '../Shared/Navbar/Navbar';
+import LeftSideNav from '../Shared/LeftSideNav/LeftSideNav';
+import RightSideNav from '../Shared/RightSideNav/RightSideNav';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Navbar></Navbar>
-            <h2 className='text-2xl font-poppins'>This is home</h2>
+
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
+                <div className='border'> 
+                    <LeftSideNav></LeftSideNav> 
+                </div>
+                <div className=' md:col-span-2 border'>
+                    <h2 className='text-4xl'>News coming soon...</h2>
+                </div>
+                <div className='border'>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,8 +1,13 @@
 
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
+
 const Header = () => {
     return (
         <div>
-            <h2 className="text-3xl">Header</h2>
+            <img src= {logo} alt="" />
+            <p className='text-lg'>Journalism Without Fear or Favour</p>
+            <p className='text-xl'>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };
