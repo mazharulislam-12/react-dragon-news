eb610ce193199a5b0995b42ce92aaa4309f2223b
mazharulislam-12 <505mazharulislam@gmail.com>
2023-10-03 00:13:54 -0700

    right side navbar cteate

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,8 +1,22 @@
+import { data } from "autoprefixer";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(()=>{
+        fetch('categories.json')    
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    },[])
+
     return (
         <div>
-             <h2 className="text-2xl">Left Side nav</h2>
+             <h2 className="text-2xl">All Caterogy {categories.length} </h2>
+             {
+                categories.map(category => <Link to={`/category/${category.id}`} className="block mb-4 ml-4 text-xl font-semibold" key={category.id}>{category.name}</Link> )
+             }
         </div>
     );
 };
